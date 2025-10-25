676879c1acd3e6a6a4f7ec9acd1179eb0955e5f3
Jonathan Palma <166739509+gitttjonzzz@users.noreply.github.com>
2025-10-25 00:43:08 +0000

    light styling

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,32 @@
 import React from "react";
-
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const[lightOn, setLightOn]=React.useState(false)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const ChangeLight =()=>{
+	setLightOn(!lightOn)
+}
+
+	return (
+		<div className="light">
+            <div className="container text-center">
+				
+				<div className="red" onClick={()=>lightOn ? ChangeLight() :style{{redStyle}}>
+					
+				</div>
+				<div className="yellow">
+					
+				</div>
+				<div className="green">
+					
+				</div>
+        	</div>
+			
 		</div>
+
 	);
 };
 
