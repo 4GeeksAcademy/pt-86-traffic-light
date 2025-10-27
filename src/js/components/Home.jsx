bc019ac04d01425d2e732fddf68f69f303656e9d
Jonathan Palma <166739509+gitttjonzzz@users.noreply.github.com>
2025-10-27 17:34:47 +0000

    Traffic Light

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,23 +4,37 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const[lightOn, setLightOn]=React.useState(false)
+	const[lightOn, setLightOn]=React.useState("color")
+	const[purpleLight, setPurpleLight]=React.useState(false)
+	
 
 const ChangeLight =()=>{
 	setLightOn(!lightOn)
 }
+const PurpleLight=()=>{
+	setPurpleLight(!purpleLight)
+}
 
 	return (
-		<div className="light">
+		<div className="light" >
+			
             <div className="container text-center">
 				
-				<div className="red" onClick={()=>lightOn ? ChangeLight() :style{{redStyle}}>
+				<div className={lightOn=="color"? "red glow" : 'red'} onClick={()=>ChangeLight()}>
 					
 				</div>
-				<div className="yellow">
+				<div className={lightOn=="color"? "yellow glow" : 'yellow'} onClick={()=>ChangeLight()}>
 					
 				</div>
-				<div className="green">
+				<div className={lightOn=="color"? "green glow" : 'green'} onClick={()=>ChangeLight()}>
+					
+				</div>
+				<div>
+					{purpleLight ? (
+						<div className="purple glow" onClick={()=>setPurpleLight(false)}></div>
+					) : (
+						<button onClick={()=>PurpleLight()}>Add Purple Light</button>
+					)}
 					
 				</div>
         	</div>
