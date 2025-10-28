36b3a2e7c91f67719a1ddb3e79847e8bb457a6ff
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2025-10-28 00:04:52 +0000

    helped out a little

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,44 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const[lightOn, setLightOn]=React.useState("color")
-	const[purpleLight, setPurpleLight]=React.useState(false)
+	const [lightOn, setLightOn] = React.useState("red")
+	const [purpleLight, setPurpleLight] = React.useState(false)
+
+
+	const ChangeLight = (color) => {
+		setLightOn(color)
+	}
+	const PurpleLight = () => {
+		setPurpleLight(!purpleLight)
+	}
+
+	useEffect(() => {
+		let colors = ['red', 'yellow', 'green'];
+		let index = 0
+		setInterval(() => {
+			setLightOn(colors[index])
+			if (index == colors.length - 1) {
+				index = 0;
+			}
+			else {
+				index = index + 1
+			}
+		}, 3000)
+	}, [])
 	
-
-const ChangeLight =()=>{
-	setLightOn(!lightOn)
-}
-const PurpleLight=()=>{
-	setPurpleLight(!purpleLight)
-}
-
 	return (
 		<div className="light" >
-			
-            <div className="container text-center">
-				
-				<div className={lightOn=="color"? "red glow" : 'red'} onClick={()=>ChangeLight()}>
-					
+
+			<div className="container text-center">
+
+				<div className={lightOn == "red" ? "red glow" : 'red'} onClick={() => ChangeLight("red")}>
+
 				</div>
-				<div className={lightOn=="color"? "yellow glow" : 'yellow'} onClick={()=>ChangeLight()}>
-					
+				<div className={lightOn == "yellow" ? "yellow glow" : 'yellow'} onClick={() => ChangeLight("yellow")}>
+
 				</div>
-				<div className={lightOn=="color"? "green glow" : 'green'} onClick={()=>ChangeLight()}>
-					
+				<div className={lightOn == "green" ? "green glow" : 'green'} onClick={() => ChangeLight("green")}>
+
 				</div>
 				<div>
 					{purpleLight ? (
-						<div className="purple glow" onClick={()=>setPurpleLight(false)}></div>
+						<div className="purple glow" onClick={() => setPurpleLight(false)}></div>
 					) : (
-						<button onClick={()=>PurpleLight()}>Add Purple Light</button>
+						<button onClick={() => PurpleLight()}>Add Purple Light</button>
 					)}
-					
+
 				</div>
-        	</div>
-			
+			</div>
+
 		</div>
 
 	);
